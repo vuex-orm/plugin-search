97c4d46f2794c1326785410eec94ff72e06e768a
Cue <1493221+cuebit@users.noreply.github.com>
2020-04-02 01:58:57 +0100

    build: new build system (#10)

--- src/index.cjs.ts
+++ src/index.cjs.ts
@@ -4,8 +4,6 @@ import Components from './contracts/Components'
 import Options from './contracts/Options'
 import VuexORMSearch from './VuexORMSearch'
 
-export { Options }
-
 export default {
   install(components: Components, installOptions: Options): void {
     new VuexORMSearch(components, installOptions).plugin()
