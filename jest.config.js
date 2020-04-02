48b76b3a2cd443010f14cf8f1bf1a5a630e5c3f3
Cue <1493221+cuebit@users.noreply.github.com>
2020-04-02 02:10:38 +0100

    build(jest): extract from package to config file

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  rootDir: __dirname,
+  globals: {
+    __DEV__: true
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)$': '<rootDir>/test/$1'
+  },
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.cjs.ts']
+}
